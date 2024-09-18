2e7401e7fff2a91df686cf01e5f044a3fac6ee89
wkao2077 <161785818+wkao2077@users.noreply.github.com>
2024-09-18 12:15:13 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,16 +7,16 @@ sed -i 's/192.168.1.1/192.168.1.251/g' openwrt/package/base-files/files/bin/conf
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 添加额外插件
-git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
-git_sparse_clone main https://github.com/linkease/nas-packages-luci luci/luci-app-ddnsto
-git_sparse_clone master https://github.com/linkease/nas-packages network/services/ddnsto
+git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff openwrt/package/luci-app-poweroff
+git_sparse_clone main https://github.com/linkease/nas-packages-luci openwrt/luci/luci-app-ddnsto
+git_sparse_clone master https://github.com/linkease/nas-packages openwrt/network/services/ddnsto
 
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages openwrt/package/openwrt-passwall
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall openwrt/package/luci-app-passwall
 
 
-git clone --depth=1 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-git clone --depth=1 -b js https://github.com/kenzok78/luci-theme-design package/luci-theme-design
+git clone --depth=1 https://github.com/kiddin9/luci-theme-edge openwrt/package/luci-theme-edge
+git clone --depth=1 -b js https://github.com/kenzok78/luci-theme-design openwrt/package/luci-theme-design
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
