2649d52303ce369b33625c5863e564150e5df7a1
wkao2077 <161785818+wkao2077@users.noreply.github.com>
2024-09-18 22:29:14 +0800

    Update custom-iwt.sh

--- custom-iwt.sh
+++ custom-iwt.sh
@@ -6,8 +6,7 @@ sed -i 's/192.168.1.1/192.168.1.251/g' openwrt/package/base-files/files/bin/conf
 # 添加额外插件
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff openwrt/package/feeds/luci/luci-app-poweroff
 
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages openwrt/package/openwrt-passwall
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall openwrt/package/luci-app-passwall
+# git clone --depth=1 -b main https://github.com/fw876/helloworld openwrt/package/feeds/luci/luci-app-ssr-plus
 
 
 git clone --depth=1 -b js https://github.com/kenzok78/luci-theme-design openwrt/package/feeds/luci/luci-theme-design
