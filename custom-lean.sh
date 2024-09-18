44aced643bdcefc22be852e3663b9710e82d9d90
wkao2077 <161785818+wkao2077@users.noreply.github.com>
2024-09-18 16:27:52 +0800

    Update custom-lean.sh

--- custom-lean.sh
+++ custom-lean.sh
@@ -5,7 +5,7 @@ sed -i 's/192.168.1.1/192.168.1.251/g' openwrt/package/base-files/files/bin/conf
 
 
 # 添加额外插件
-git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff openwrt/package/luci-app-poweroff
+git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff openwrt/package/feeds/luci/luci-app-poweroff
 
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages openwrt/package/openwrt-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall openwrt/package/luci-app-passwall
