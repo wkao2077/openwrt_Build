b5019b680ff6474b2046713aec1fa07820a95c53
wkao2077 <161785818+wkao2077@users.noreply.github.com>
2024-09-19 08:50:31 +0800

    Update custom-lean.sh

--- custom-lean.sh
+++ custom-lean.sh
@@ -17,5 +17,5 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall openwrt/packag
 # git clone --depth=1 -b main https://github.com/fw876/helloworld openwrt/package/feeds/luci/luci-app-ssr-plus
 
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon openwrt/package/feeds/luci/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config openwrt/package/feeds/luci/luci-app-argon-config
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-app-argon-config openwrt/package/feeds/luci/luci-app-argon-config
 git clone --depth=1 https://github.com/kenzok78/luci-theme-design openwrt/package/feeds/luci/luci-theme-design
