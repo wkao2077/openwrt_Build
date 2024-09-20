fda14703c1eee25c2adfbd0b3ab65eff0a2284a5
wkao2077 <161785818+wkao2077@users.noreply.github.com>
2024-09-20 19:52:21 +0800

    Update custom-iwt.sh

--- custom-iwt.sh
+++ custom-iwt.sh
@@ -5,6 +5,9 @@ sed -i 's/192.168.1.1/192.168.1.251/g' openwrt/package/base-files/files/bin/conf
 
 # 添加额外插件
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff openwrt/package/feeds/luci/luci-app-poweroff
+git clone --depth=1 -b master https://github.com/linkease/nas-packages openwrt/package/nas
+git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci openwrt/package/nas_luci
+
 
 # git clone --depth=1 https://github.com/fw876/helloworld openwrt/package/feeds/luci/luci-app-ssr-plus
 
