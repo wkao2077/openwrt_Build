7ee0a73cc93637406d474e78dd95e371db6f6e80
wkao2077 <161785818+wkao2077@users.noreply.github.com>
2024-10-21 02:02:21 +0800

    Update iwt-x86.sh

--- iwt-x86.sh
+++ iwt-x86.sh
@@ -21,7 +21,7 @@ rm -rf openwrt/package/nas/network/services/webdav2
 
 # 科学上网
 # git clone --depth=1 https://github.com/fw876/helloworld openwrt/package/feeds/luci/luci-app-ssr-plus
-git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall2 openwrt/package/feeds/luci/luci-app-passwall2
+# git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall2 openwrt/package/feeds/luci/luci-app-passwall2
 
 # 主题
 # git clone --depth=1 -b js https://github.com/kenzok78/luci-theme-design openwrt/package/feeds/luci/luci-theme-design
