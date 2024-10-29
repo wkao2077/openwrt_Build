40807399b044f2bb94ca8392fc57942414609e3b
wkao2077 <161785818+wkao2077@users.noreply.github.com>
2024-10-29 22:07:20 +0800

    Update iwt-x86-2.sh

--- iwt-x86-2.sh
+++ iwt-x86-2.sh
@@ -4,11 +4,11 @@
 sed -i 's/192.168.1.1/192.168.1.251/g' openwrt/package/base-files/files/bin/config_generate
 
 # 添加额外插件
-# git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff openwrt/package/feeds/luci/luci-app-poweroff
 git clone --depth=1 https://github.com/sirpdboy/luci-app-poweroffdevice openwrt/package/feeds/luci/luci-app-poweroffdevice
 
 
 # 科学上网
+# git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall2 openwrt/package/feeds/luci/luci-app-passwall2
 # git clone --depth=1 -b main https://github.com/morytyann/OpenWrt-mihomo openwrt/package/mihomo
 
 # 主题
