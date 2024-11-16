dee6c39786bb3e83db50f77b0916a875d6e9fc41
wkao2077 <161785818+wkao2077@users.noreply.github.com>
2024-11-16 16:23:20 +0800

    Update iwt-x86.sh

--- iwt-x86.sh
+++ iwt-x86.sh
@@ -6,19 +6,6 @@ sed -i 's/192.168.1.1/192.168.1.251/g' openwrt/package/base-files/files/bin/conf
 # 添加额外插件
 git clone --depth=1 https://github.com/sirpdboy/luci-app-poweroffdevice openwrt/package/feeds/luci/luci-app-poweroffdevice
 
-# git clone --depth=1 -b master https://github.com/linkease/nas-packages openwrt/package/nas
-# git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci openwrt/package/nas_luci
-# rm -rf openwrt/package/nas_luci/luci/luci-app-istorex
-# rm -rf openwrt/package/nas_luci/luci/luci-app-quickstart
-# rm -rf openwrt/package/nas_luci/luci/luci-app-unishare
-# rm -rf openwrt/package/nas_luci/luci/luci-app-istoreenhance
-# rm -rf openwrt/package/nas_luci/luci/luci-app-linkease
-# rm -rf openwrt/package/nas_luci/luci/luci-nginxer
-# rm -rf openwrt/package/nas_luci/luci/luci-lib-iform
-# rm -rf openwrt/package/nas/network/services/quickstart
-# rm -rf openwrt/package/nas/network/services/unishare
-# rm -rf openwrt/package/nas/network/services/webdav2
-
 # 科学上网
 # git clone --depth=1 https://github.com/fw876/helloworld openwrt/package/feeds/luci/luci-app-ssr-plus
 # git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall2 openwrt/package/feeds/luci/luci-app-passwall2
