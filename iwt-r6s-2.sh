596d3c9fb27c16f0ddf70373bdeeb44e976becd0
wkao2077 <161785818+wkao2077@users.noreply.github.com>
2024-11-17 00:18:36 +0800

    Update iwt-r6s-2.sh

--- iwt-r6s-2.sh
+++ iwt-r6s-2.sh
@@ -7,8 +7,8 @@ sed -i 's/192.168.1.1/192.168.77.2/g' openwrt/package/base-files/files/bin/confi
 
 
 # 科学上网
-# git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall2 openwrt/package/feeds/luci/luci-app-passwall2
-git clone --depth=1 -b main https://github.com/morytyann/OpenWrt-mihomo openwrt/package/luci-app-mihomo
+git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall2 openwrt/package/feeds/luci/luci-app-passwall2
+# git clone --depth=1 -b main https://github.com/morytyann/OpenWrt-mihomo openwrt/package/luci-app-mihomo
 
 # 主题
 # git clone --depth=1 -b js https://github.com/kenzok78/luci-theme-design openwrt/package/feeds/luci/luci-theme-design
